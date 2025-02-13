034f0d47f830ad407d004fc5f89970f83781efdc
Cody J <codywj777@gmail.com>
2025-02-13 08:45:40 -0800

    updates build command, removes 'developer'

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pdflatex -synctex=1 -interaction=nonstopmode main.tex
-mv main.pdf $HOME/career/CodyJohnsonCV.pdf
+
+
+case "$1" in
+    "sc") file="main.sc" ;;
+    *) file="main" ;;
+esac
+
+pdflatex -synctex=1 -interaction=nonstopmode $file.tex
+mv $file.pdf $HOME/career/cv_$file.pdf
