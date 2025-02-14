6d7af64ce2dc3cf42e32a64ca68e3da8e18b7ed8
Cody J <codywj777@gmail.com>
2025-02-13 22:11:32 -0800

    cl

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 
 case "$1" in
     "sc") file="main.sc" ;;
+    "cl") file="cover-letter" ;;
     *) file="main" ;;
 esac
 
 pdflatex -synctex=1 -interaction=nonstopmode $file.tex
-mv $file.pdf $HOME/career/cv_$file.pdf
+mv $file.pdf $HOME/career/$file.pdf
